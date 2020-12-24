e5e567734079bb9ab0fca4f53fee291a84280f91
allen-huang9 <ligerx066@gmail.com>
2020-12-23 16:10:28 -0800

    Displays data on frontend

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entryList: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/entries/' + 1)
+      .then(response => response.json())
+      .then(jsonData => {
+        this.setState({
+          entryList: jsonData
+        });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+
+    const entryListItem = this.state.entryList.map(entry => {
+      return (
+        <div key={entry.entryId} className="singleEntry">
+          <div>{entry.date}</div>
+          <div>{entry.amount}</div>
+        </div>
+      );
+    });
+
+    return (
+      <>
+        <header>
+          APP NAME
+        </header>
+        <div className="list-container">
+          {entryListItem}
+        </div>
+      </>
+    );
+  }
 }
+export default Home;
--- server/index.js
+++ server/index.js
@@ -16,12 +16,13 @@ const jsonMiddleware = express.json();
 app.use(staticMiddleware);
 app.use(jsonMiddleware);
 
-app.get('/api/entries', (req, res, next) => {
+app.get('/api/entries/:userId', (req, res, next) => {
 
   const sql = `select "entryId", "amount", "description", TO_CHAR("date" :: DATE, 'mm/dd/yyyy') as "date", "name" as "category"
                from "entry" join "category" using ("categoryId")
                where "userId" = $1`;
-  const { userId } = req.body;
+  const userId = parseInt(req.params.userId, 10);
+
   if (!userId || userId < 0 || !Number.isInteger(userId)) {
     throw new ClientError(401, 'User must be logged in');
   }
