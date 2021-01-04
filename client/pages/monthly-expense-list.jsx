2eef7c1595bfae6fba84d3243ff18ee71655bf49
allen-huang9 <ligerx066@gmail.com>
2021-01-04 15:34:08 -0800

    Client sends GET request to server and displays the data

--- client/pages/monthly-expense-list.jsx
+++ client/pages/monthly-expense-list.jsx
@@ -9,15 +9,53 @@ class MonthlyExpenseList extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch('/api/monthly-expense/' + 1)
+      .then(response => response.json())
+      .then(monthlyExpenseList => {
+        this.setState({ monthlyExpenseList });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
+
+    const monthlyExpenses = this.state.monthlyExpenseList.map(monthlyExpense => {
+
+      const date = monthlyExpense.date;
+
+      return (
+        <tr key={date}>
+          <td>{date}</td>
+          <td className="amount-td">${monthlyExpense.sum}</td>
+          <td>
+            <a className="view-single-entry-button"
+              href={''}>
+              View
+            </a>
+          </td>
+        </tr>
+      );
+    });
+
     return (
       <>
         <header>
           <Menu />
-          <p className="header-text">APP Name</p>
+          <p className="header-text">Monthly Expenses</p>
         </header>
-        <div>
-          monthly expense place holder
+        <div className="list-container">
+          <table className="monthly-expense-list-table">
+            <thead>
+              <tr>
+                <th className="text-center">Date</th>
+                <th className="amount-td">Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {monthlyExpenses}
+            </tbody>
+          </table>
         </div>
       </>
     );
