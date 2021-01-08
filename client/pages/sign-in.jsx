e7570de9403bb500aa63d51b3fc68a294d2eb59d
allen-huang9 <ligerx066@gmail.com>
2021-01-07 18:19:12 -0800

    Added a sign in button and added css to the form elements

--- client/pages/sign-in.jsx
+++ client/pages/sign-in.jsx
@@ -7,21 +7,30 @@ class SignIn extends React.Component {
         <header>
           <p className="header-text">Sign In</p>
         </header>
-        <form>
-          <label htmlFor="username">Username</label>
-          <input className="form-control"
-            type="text"
-            name="username"
-            id="username">
-          </input>
+        <div className="sign-in-form-container pt-5 d-flex justify-content-center">
+          <form className="w-75">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input className="form-control"
+                type="text"
+                name="username"
+                id="username">
+              </input>
+            </div>
 
-          <label htmlFor="password">Password</label>
-          <input className="form-control"
-            type="password"
-            name="password"
-            id="password">
-          </input>
-        </form>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input className="form-control"
+                type="password"
+                name="password"
+                id="password">
+              </input>
+            </div>
+            <div className="d-flex justify-content-center">
+              <button className="btn btn-success w-50">Sign In</button>
+            </div>
+          </form>
+        </div>
       </>
     );
   }
