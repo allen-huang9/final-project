299a31bd54d6d95a3c12b8af39688b42936c3917
allen-huang9 <ligerx066@gmail.com>
2021-01-08 11:22:23 -0800

    Added a constructor, handleCHange method and handleSubmit method

--- client/pages/sign-in.jsx
+++ client/pages/sign-in.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
 
 class SignIn extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const inputName = event.target.name;
+    const updatedState = this.state;
+    updatedState[inputName] = event.target.value;
+    this.setState({ [inputName]: updatedState[inputName] });
+  }
+
+  handleSubmit() {
+    const init = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    };
+    fetch('/apis/sign-in', init);
+  }
+
   render() {
     return (
       <>
         <header>
-          <p className="header-text">Sign In</p>
+          <p className="header-text">Money Bluff</p>
         </header>
         <div className="sign-in-form-container pt-5 d-flex justify-content-center">
-          <form className="w-75">
+          <form className="w-75" onSubmit={this.handleSubmit}>
             <div className="form-group">
               <label htmlFor="username">Username</label>
               <input className="form-control"
                 type="text"
                 name="username"
-                id="username">
+                id="username"
+                onChange={this.handleChange}>
               </input>
             </div>
 
@@ -23,7 +53,8 @@ class SignIn extends React.Component {
               <input className="form-control"
                 type="password"
                 name="password"
-                id="password">
+                id="password"
+                onChange={this.handleChange}>
               </input>
             </div>
             <div className="d-flex justify-content-center">
