0382002a0692469fb5b15552d1586d38b7630021
allen-huang9 <ligerx066@gmail.com>
2021-02-19 15:23:00 -0800

    Fixed login redirection

--- client/app.jsx
+++ client/app.jsx
@@ -58,12 +58,13 @@ export default class App extends React.Component {
     const { user, signedToken } = result;
     window.localStorage.setItem('money-token', signedToken);
     window.location.hash = 'home';
-    this.setState({ user, signedToken });
+    this.setState({ user, signedToken, isAuthorizing: false });
+
   }
 
   handleSignOut() {
     window.localStorage.removeItem('money-token');
-    this.setState({ user: null, signedToken: null });
+    this.setState({ user: null, signedToken: null, isAuthorizing: true });
     window.location.hash = '';
   }
 
